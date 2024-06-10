e1cd1e2b71428b47c67dca636aab87e3118cc736
Bartosz Dybowski <01161292@pw.edu.pl>
2024-06-10 16:41:58 +0200

    Fixed tests

--- tests/JungleMctsDuels.Tests/Program.cs
+++ tests/JungleMctsDuels.Tests/Program.cs
@@ -1,13 +1,13 @@
 ﻿using JungleMctsDuels.Tests;
 
 await Task.WhenAll(
-            Task.Run(() => MctsUctVsMctsUct.Run()),
+            Task.Run(MctsUctVsMctsUct.Run),
             Task.Run(MctsUctVsMctsBeam.Run),
             Task.Run(MctsUctVsAlphaBeta.Run),
             Task.Run(AlphaBetaVsAlphaBeta.Run),
             Task.Run(MctsBeamVsAlphaBeta.Run),
             Task.Run(MctsBeamVsMctsBeam.Run)
-            Task.Run(AlphaBetaVsReflexiveMcts.Run),
+            Task.Run(AlphaBetaVsReflexiveMcts.Run)
             Task.Run(MctsBeamVsReflexiveMcts.Run),
             Task.Run(MctsUctVsReflexiveMcts.Run),
             Task.Run(ReflexiveMctsVsReflexiveMcts.Run)
